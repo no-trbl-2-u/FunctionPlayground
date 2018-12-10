7852ffc1d85fa4499bd8d81653711ca2e38a2d5f
TJ <no.trbl.2.u@gmail.com>
2018-12-10 15:00:14 -0500

    Added Filter function

--- Main.hs
+++ Main.hs
@@ -1,8 +1,27 @@
 absv :: Int -> Int
 absv n = if n > 0 then n else -n
 
+-- Partial Application to map
 doubleSmallNumber :: Int -> Int
 doubleSmallNumber n = if n < 100 then n * 2 else n
 
 doubleSN :: [Int] -> [Int]
 doubleSN = map (doubleSmallNumber)
+
+inc :: Num a => a -> a
+inc n = n + 1
+
+incList :: Num a => [a] -> [a]
+incList = map inc
+
+-- Filter Function
+isChris :: [Char] -> Bool
+isChris str = str == "Chris"
+
+nameList :: [String]
+nameList = ["Steve", "Chris", "Nathan", "Josh"]
+
+lookForChris :: [String] -> [[Char]]
+lookForChris = filter isChris
+
+
